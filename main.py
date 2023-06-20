bc877746f0dd62b6dc6ed4efbf8ffef6fdbf0774
guilhermelmello <guilhermelmello@yahoo.com.br>
2023-06-19 21:25:52 -0300

    Add tokenization support

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from transformers_finetuning import tasks
 from transformers_finetuning import utils
 
+from transformers import AutoTokenizer
+
 import datasets
 
 
@@ -15,14 +17,41 @@ task_kwargs = dict()
 if args.text_pair_column is not None:
     task_kwargs['text_pair_column'] = args.text_pair_column
 
+# map dataset's columns to predefined names
 task = tasks.get_task(
     task_name=args.task_name,
     text_column=args.text_column,
     label_column=args.label_column,
     **task_kwargs)
 
+# load a hugginface's dataset
 dataset = datasets.load_dataset(
     path=args.dataset_path,
     name=args.dataset_config,
     task=task
+)
+
+
+# -------------------- #
+# Dataset Tokenization #
+# -------------------- #
+
+# load tokenizer and map function
+tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+to_tokens = tasks.get_dataset_mapper(
+    tokenizer=tokenizer,
+    text_pairs=args.text_pair_column)
+
+# text columns to drop
+if args.text_pair_column is not None:
+    drop_columns = ['text', 'text_pair']
+else:
+    drop_columns = ['text']
+
+# dataset tokenization
+dataset = dataset.map(
+    function=to_tokens,
+    batched=True,
+    batch_size=args.dataprep_batch_size,
+    remove_columns=drop_columns
 )
--- transformers_finetuning/utils.py
+++ transformers_finetuning/utils.py
@@ -7,7 +7,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Automatic finetuning for Transformer models.")
 
-    # dataset arguments
+    # DATASET ARGUMENTS
     parser.add_argument(
         "--dataset_path",
         help="Path or name of the dataset. Used by `datasets.load_dataset'",
@@ -24,29 +24,42 @@ def parse_arguments():
     parser.add_argument(
         "--text_column",
         help="Name of the text column in the dataset.",
+        type=str,
         default="text")
 
     parser.add_argument(
         "--text_pair_column",
         help="Name of the text column pair. Used on pair of sequences tasks.",
+        type=str,
         default=None)
 
     parser.add_argument(
         "--label_column",
         help="Name of the target column.",
+        type=str,
         default="label")
 
     parser.add_argument(
         "--dataprep_batch_size",
         help=("Batch size for dataset tokenization."
               "For training, use `batch_size'"),
+        type=int,
         default=1000)
 
     parser.add_argument(
         "--task_name",
         help="The task defining the a Machine Learning problem/setup.",
+        type=str,
         default="text-classification",
         choices=get_available_tasks())
+
+    # MODEL ARGUMENTS
+    parser.add_argument(
+        "--model_path",
+        help="",
+        type=str,
+        default=None,
+        required=True)
 
     args = parser.parse_args()
     return args
