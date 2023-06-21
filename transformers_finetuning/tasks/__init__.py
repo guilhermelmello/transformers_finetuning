06e59dbd8622bccc04740c6e228e7b9d53ee5281
guilhermelmello <guilhermelmello@yahoo.com.br>
2023-06-21 11:17:12 -0300

    Add AutoModel for text classification tasks.

--- transformers_finetuning/tasks/__init__.py
+++ transformers_finetuning/tasks/__init__.py
@@ -1,6 +1,8 @@
-from . import text_classification as text_classification
-from . import text_regression as text_regression
+from . import text_classification
+from . import text_regression
 from .mappers import TextMapper
+
+from transformers import AutoModelForSequenceClassification
 
 
 _available_tasks = {
@@ -13,6 +15,16 @@ _available_tasks = {
     "text-pair-regression": text_regression.TextPairRegression,
 }
 
+_auto_models = {
+    # for text classification
+    "text-classification": AutoModelForSequenceClassification,
+    "text-pair-classification": AutoModelForSequenceClassification,
+
+    # for text regression TODO
+    # "text-regression":
+    # "text-pair-regression":
+}
+
 
 def get_available_tasks():
     return list(_available_tasks.keys())
@@ -23,6 +35,13 @@ def get_task(task_name, *args, **kwargs):
         raise ValueError(f"'{task_name}' is not a valid task.")
     else:
         return _available_tasks[task_name](*args, **kwargs)
+
+
+def get_automodel(task_name):
+    if task_name not in _auto_models:
+        raise ValueError(f"Could not find an AutoModel for '{task_name}'.")
+    else:
+        return _auto_models[task_name]
 
 
 def get_dataset_mapper(tokenizer, text_pairs=False):
